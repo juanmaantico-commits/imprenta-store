cf201f1bae2378e92df6887f74a1b36d0f31ad4b
Juan <juan.antico@gmail.com>
2026-05-09 18:42:29 -0300

    fix espacio negro hero

--- src/App.jsx
+++ src/App.jsx
@@ -1478,8 +1478,6 @@ const handleLoginAdmin = (pass) => {
         <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15v-4H7l5-8v4h4l-5 8z"/></svg>
         Tienda Nube
       </a>
-    </div>
-  </div>
 </div>
           </div>
         </div>
